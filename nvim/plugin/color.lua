54cb110e4ca9c3a49e51705e8ed098b0247d01cc
JakobSever <jakob1.sever@gmail.com>
2024-04-17 22:11:18 +0200

    Fixed highlight color

--- nvim/plugin/color.lua
+++ nvim/plugin/color.lua
@@ -1,6 +1,8 @@
-function ColorMyPencils(color)
+function ColorMyPencils()
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+	vim.cmd('highlight Visual  guifg=#000000 guibg=#FFFFFF gui=none');
 end
 
 ColorMyPencils()
